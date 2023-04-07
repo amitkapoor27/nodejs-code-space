8f5dc79adafebc4fbd3ee93cd19883d6abca2699
Amit Kapoor <amit.kapoor27@gmail.com>
2023-04-07 12:56:41 +0530

    Update count-repetive-char-string.js
    more efficient code.

--- count-repetive-char-string.js
+++ count-repetive-char-string.js
@@ -1,15 +1,14 @@
 const example1 = 'abacadadefada44445555555';
 
 function freqChar(abc){
-    let b =[];
-    for(let a in abc){
-        if(!Number(abc[a])){
-            if(b[abc[a]]){  
-                b[abc[a]] += 1;
-            }
-            else {
-                b[abc[a]] = 1;
-            }
+    const b =[];
+    for(const a of example1){
+        if(Number(a)) continue;
+        if(b[a]){
+            b[a] +=1;
+        }
+        else {
+            b[a]=1;
         }
     }
     for(let k in b){
