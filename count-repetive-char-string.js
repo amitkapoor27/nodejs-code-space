016340d844e6e5f0865dc8eda88773fd3a57b5b8
Amit Kapoor <amit.kapoor27@gmail.com>
2023-04-07 13:13:08 +0530

    Update count-repetive-char-string.js

--- count-repetive-char-string.js
+++ count-repetive-char-string.js
@@ -4,12 +4,11 @@ function freqChar(abc){
     const b =[];
     for(const a of example1){
         if(Number(a)) continue;
-        if(b[a]){
-            b[a] +=1;
+        if(!b[a]){
+            b[a]=0;
         }
-        else {
-            b[a]=1;
-        }
+            b[a]++;
+        
     }
     for(let k in b){
         
