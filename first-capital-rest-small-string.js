b6f83da16d2c2f0a798f6a86bf36041000c3db2c
Amit Kapoor <amit.kapoor27@gmail.com>
2023-04-07 13:34:47 +0530

    optimized code

--- first-capital-rest-small-string.js
+++ first-capital-rest-small-string.js
@@ -1,9 +1,8 @@
 const a = ["deLhi", "kolKATa", "ChenNai", "gOa", "gUWAhati"];
-const len = a.length;
+let b =[];
 
-for (let i = 0; i < len; i++) {
-    const str = a[i];
-    let fle = str[0].toUpperCase();
-    let rle = str.slice(1).toLowerCase();
-    console.log(fle+rle);
+for(k of a){
+    let c = k.slice(1).toLowerCase();
+    b.push(k[0].toUpperCase().concat(c));
 }
+console.log(b);
